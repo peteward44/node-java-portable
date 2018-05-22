d4ae1768b267e5ef66774a275c59909eeba49d75
Pete Ward <peteward44@gmail.com>
2018-05-22 21:49:42 +0100

    postinstall abstracted into own function for tests

--- postInstall.js
+++ postInstall.js
@@ -1,19 +1,14 @@
-const path = require( 'path' );
-const fs = require( 'fs' );
-const getVersions = require( './lib/getVersions.js' );
+const postInstall = require( './lib/postInstall.js' );
 
 function handleError( err ) {
 	console.error( err );
 	process.exit( 1 );	
 }
 
-function postInstall() {
+function install() {
 	// record version of node.js and JDK / JRE, which is then used to check when executing the actual module
-	return getVersions()
-	.then( versions => {
-		fs.writeFileSync( path.join( __dirname, 'versions.json' ), JSON.stringify( versions, null, 2 ), 'utf8' );
-	} )
+	return postInstall()
 	.catch( err => handleError( err ) );
 }
 
-postInstall();
+install();
--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
+const postInstall = require( '../lib/postInstall.js' );
 const javaPortable = require( '..' );
 const chai = require( 'chai' );
 
 describe( 'node-java-portable', () => {
 	it( 'basic test', () => {
-		return javaPortable().then( java => {
+		return postInstall()
+		.then( () => javaPortable() )
+		.then( java => {
 			chai.assert( !!java );
 		} );
 	} );
