981012a553d33dd0f2a0c393733a74aa35bc8395
Pete Ward <peteward44@gmail.com>
2019-05-15 00:04:31 +0100

    wip

--- postInstall.js
+++ postInstall.js
@@ -1,14 +1,15 @@
 const postInstall = require( './lib/postInstall.js' );
 
-function handleError( err ) {
-	console.error( err );
-	process.exit( 1 );	
-}
 
 function install() {
 	// record version of node.js and JDK / JRE, which is then used to check when executing the actual module
-	return postInstall()
-	.catch( err => handleError( err ) );
+	try {
+		postInstall();
+	}
+	catch( err ) {
+		console.error( err );
+		process.exit( 1 );
+	}
 }
 
 install();
