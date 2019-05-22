6a24a1d22925645bba8179d53bb8eddbd6251d88
Pete Ward <pete.ward@inseinc.com>
2019-05-22 15:29:46 +0100

    wip

--- test/test.js
+++ test/test.js
@@ -17,38 +17,42 @@ const testOptions = {
 };
 
 describe( 'node-java-portable', () => {
-	beforeEach( () => {
-		try {
-			if ( !fs.existsSync( testRootDir ) ) {
-				fs.mkdirSync( testRootDir );
-			}
-			if ( fs.existsSync( testVersionsJsonPath ) ) {
-				fs.unlinkSync( testVersionsJsonPath );
-			}
-			if ( !fs.existsSync( testNodeJavaPath ) ) {
-				fs.mkdirSync( testNodeJavaPath );
-			}
-		} catch ( err ) {
-			console.error( `Error deleting "${testVersionsJsonPath}"` );
-			console.error( err );
-		}
-	} );
+	// beforeEach( () => {
+		// try {
+			// if ( !fs.existsSync( testRootDir ) ) {
+				// fs.mkdirSync( testRootDir );
+			// }
+			// if ( fs.existsSync( testVersionsJsonPath ) ) {
+				// fs.unlinkSync( testVersionsJsonPath );
+			// }
+			// if ( !fs.existsSync( testNodeJavaPath ) ) {
+				// fs.mkdirSync( testNodeJavaPath );
+			// }
+		// } catch ( err ) {
+			// console.error( `Error deleting "${testVersionsJsonPath}"` );
+			// console.error( err );
+		// }
+	// } );
 
-	it( 'post install creates versions.json which contains valid json', () => {
-		chai.assert( !fs.existsSync( testVersionsJsonPath ) );
-		return postInstall( testOptions )
-		.then( () => {
-			chai.assert( fs.existsSync( testVersionsJsonPath ) );
-			const json = JSON.parse( fs.readFileSync( testVersionsJsonPath, 'utf8' ) );
-			chai.expect( json ).to.be.a( 'object' );
-		} );
-	} );
+	// it( 'post install creates versions.json which contains valid json', () => {
+		// chai.assert( !fs.existsSync( testVersionsJsonPath ) );
+		// return postInstall( testOptions )
+		// .then( () => {
+			// chai.assert( fs.existsSync( testVersionsJsonPath ) );
+			// const json = JSON.parse( fs.readFileSync( testVersionsJsonPath, 'utf8' ) );
+			// chai.expect( json ).to.be.a( 'object' );
+		// } );
+	// } );
 
-	it( 'basic test', () => {
-		return postInstall( testOptions )
-		.then( () => javaPortable( testOptions ) )
-		.then( java => {
-			chai.assert( !!java ); // chai.expect( java ).is( 'object' ) doesn't work
-		} );
-	} );
+	// it( 'basic test', () => {
+		// return postInstall( testOptions )
+		// .then( () => javaPortable( testOptions ) )
+		// .then( java => {
+			// chai.assert( !!java ); // chai.expect( java ).is( 'object' ) doesn't work
+		// } );
+	// } );
+	it( 'prepare', () => {
+		process.env.JAVA_HOME = 'C:\\Program Files\\Java\\jdk1.8.0_131';
+		return javaPortable.prepare( testOptions );
+	} ).timeout( 2 * 60 * 1000 );
 } );
